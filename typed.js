6f8ae4a863c0503d69c23612733c84fb1811e441
Shubham Arun Malankar <shubham.a.m@gmail.com>
2025-02-16 16:05:49 +0530

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Full Stack Developer", "Machine Learning Developer", "Cloud Developer", "Software Developer"],
+    strings: ["Data Engineer", "Cloud Developer", "Machine Learning Developer", "Software Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
